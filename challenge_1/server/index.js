4ec723a727e93593be4548b1677829dcfc743281
sveta0112 <sveta0112@hotmail.com>
2018-12-05 13:59:26 -0800

    webpack&json-server

---
+++ challenge_1/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+var app = express();
+app.use(express.static(__dirname + '/../public/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.listen(3000, () => {
+	console.log("Listening to port 3000!");
+});
