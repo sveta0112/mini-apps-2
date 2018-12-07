02b54e54e01f7c524286e6bccf6e612d5a3e2592
sveta0112 <sveta0112@hotmail.com>
2018-12-06 18:52:37 -0800

    chart is ready

--- challenge_2/webpack.config.js
+++ challenge_2/webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
-var SRC_DIR = path.join(__dirname,'client/src');
-var DIST_DIR = path.join(__dirname,'public/dist');
 
 module.exports = {
-  mode: 'development',
-  entry: `${SRC_DIR}/index.jsx`,
+  mode : 'development',
+  entry: './client/src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    path: path.resolve(__dirname, './public/dist'),
+    filename: 'bundle.js'
   },
   module: {
-  	rules: [
-      {
-      	test:/\.jsx?$/,
-      	exclude: /(node_modules|bower_components)/,
-      	include: SRC_DIR,
-      	loader: 'babel-loader',
-      	query: {
-      		presets: ['@babel/preset-env','@babel/preset-react']
-      	}
+    rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       }
-  	]
+    ]
   }
 };
